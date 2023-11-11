01207bcb85877423f3f83053f529d3b920317926
Josthin Soto <josthin.soto.s@estudiantec.cr>
2023-11-10 21:46:59 -0600

    Servidor
    Se sube una implementación básica del Servidor que puede leer archivos JSON

---
+++ Servidor/src/main/java/auxiliares/Empleado.java
@@ -0,0 +1,23 @@
+package archivos.main.uber.auxiliares;
+
+public class Empleado {
+
+    private String carnetUsuario;
+    private String passwordUsuario;
+
+    public String getCarnetUsuario() {
+        return carnetUsuario;
+    }
+
+    public void setCarnetUsuario(String carnetUsuario) {
+        this.carnetUsuario = carnetUsuario;
+    }
+
+    public String getPasswordUsuario() {
+        return passwordUsuario;
+    }
+
+    public void setPasswordUsuario(String passwordUsuario) {
+        this.passwordUsuario = passwordUsuario;
+    }
+}
