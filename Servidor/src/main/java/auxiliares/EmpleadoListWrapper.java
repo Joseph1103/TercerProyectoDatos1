7d9e967ae87267fdbf02f8f9b265b46e0dda0869
Gustavo Calderon Perez <tuticp04@gmail.com>
2023-11-11 21:26:25 -0600

    xml2

---
+++ Servidor/src/main/java/auxiliares/EmpleadoListWrapper.java
@@ -0,0 +1,20 @@
+package auxiliares;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "empleados")
+public class EmpleadoListWrapper {
+
+    private List<Empleado> empleados;
+
+    @XmlElement(name = "empleado")
+    public List<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(List<Empleado> empleados) {
+        this.empleados = empleados;
+    }
+}
--- Servidor/src/main/java/auxiliares/XMLHandler.java
+++ Servidor/src/main/java/auxiliares/XMLHandler.java
@@ -5,21 +5,47 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class XMLHandler {
 
     public static void marshalToXML(Empleado empleado, String filePath) throws JAXBException {
-        JAXBContext context = JAXBContext.newInstance(Empleado.class);
-        Marshaller marshaller = context.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        try {
+            List<Empleado> empleados = getAllEmpleados(filePath);
 
-        marshaller.marshal(empleado, new File(filePath));
+            empleados.add(empleado);
+
+            JAXBContext context = JAXBContext.newInstance(EmpleadoListWrapper.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            EmpleadoListWrapper wrapper = new EmpleadoListWrapper();
+            wrapper.setEmpleados(empleados);
+
+            marshaller.marshal(wrapper, new File(filePath));
+        } catch (JAXBException e) {
+            e.printStackTrace();
+            throw e; // Propagate the exception to the caller
+        }
     }
 
-    public static Empleado unmarshalFromXML(String filePath) throws JAXBException {
-        JAXBContext context = JAXBContext.newInstance(Empleado.class);
-        Unmarshaller unmarshaller = context.createUnmarshaller();
+    public static List<Empleado> getAllEmpleados(String filePath) throws JAXBException {
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                return new ArrayList<>();
+            }
 
-        return (Empleado) unmarshaller.unmarshal(new File(filePath));
+            JAXBContext context = JAXBContext.newInstance(EmpleadoListWrapper.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+
+            EmpleadoListWrapper wrapper = (EmpleadoListWrapper) unmarshaller.unmarshal(file);
+
+            return wrapper.getEmpleados();
+        } catch (JAXBException e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
