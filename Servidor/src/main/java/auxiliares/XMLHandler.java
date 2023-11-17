6ab2f76722eee0d6879f2518d7508f93253cbe78
Tuti004 <130619007+Tuti004@users.noreply.github.com>
2023-11-17 09:05:57 -0600

    xml2 cambio(creo que sirve)

--- Servidor/src/main/java/auxiliares/XMLHandler.java
+++ Servidor/src/main/java/auxiliares/XMLHandler.java
@@ -1,55 +1,39 @@
 package auxiliares;
 
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.transform.Source;
-import javax.xml.transform.stream.StreamSource;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 public class XMLHandler {
 
-    public static void marshalToXML(Empleado empleado, String filePath) throws JAXBException {
-        try {
-            List<Empleado> empleados = getAllEmpleados(filePath);
+    public static void appendToXML(Empleado newEmpleado, String filePath) throws JAXBException {
+        File file = new File(filePath);
+        Empleado existingEmpleado = null;
 
-            empleados.add(empleado);
-
-            JAXBContext context = JAXBContext.newInstance(EmpleadoListWrapper.class);
-            Marshaller marshaller = context.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-            EmpleadoListWrapper wrapper = new EmpleadoListWrapper();
-            wrapper.setEmpleados(empleados);
-
-            marshaller.marshal(wrapper, new File(filePath));
-        } catch (JAXBException e) {
-            e.printStackTrace();
-            throw e;
+        // If the file exists, unmarshal its content into an object
+        if (file.exists()) {
+            existingEmpleado = unmarshalFromXML(filePath);
+        } else {
+            // If the file doesn't exist, create a new Empleado object
+            existingEmpleado = new Empleado();
         }
+
+        // Add the new data to the existing object
+        existingEmpleado.setCarnetUsuario(newEmpleado.getCarnetUsuario());
+        existingEmpleado.setPasswordUsuario(newEmpleado.getPasswordUsuario());
+
+        // Marshal the updated object to the XML file
+        JAXBContext context = JAXBContext.newInstance(Empleado.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.marshal(existingEmpleado, file);
     }
 
-    public static List<Empleado> getAllEmpleados(String filePath) throws JAXBException {
-        try {
-            File file = new File(filePath);
-            if (!file.exists()) {
-                return new ArrayList<>();
-            }
-
-            JAXBContext context = JAXBContext.newInstance(EmpleadoListWrapper.class);
-            Unmarshaller unmarshaller = context.createUnmarshaller();
-
-            JAXBElement<EmpleadoListWrapper> jaxbElement = unmarshaller.unmarshal(new StreamSource(file), EmpleadoListWrapper.class);
-            EmpleadoListWrapper wrapper = jaxbElement.getValue();
-
-            return wrapper.getEmpleados();
-        } catch (JAXBException e) {
-            e.printStackTrace();
-            throw e;
-        }
+    public static Empleado unmarshalFromXML(String filePath) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(Empleado.class);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        return (Empleado) unmarshaller.unmarshal(new File(filePath));
     }
 }
